ce7c60e38fdc3de7a33ca60827bf7fe478e9ea2e
atiqur <atiqur@itconquest.com>
2022-04-06 14:00:17 +0600

    Eloquent-One-to-Many

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'data' => $users
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json($user->load('posts'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        // $profile = $user->profile()->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+        ], 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $destory = $user->load('posts')->delete();
+
+        if($destory){
+            return response()->json([
+                'status' => 'success',
+                'message' => "ok"
+            ], 200);
+        }
+        
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        
+        return [
+            'user_id' => User::all()->random()->id,
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->paragraphs(3, true)
+        ];
+    }
+}
